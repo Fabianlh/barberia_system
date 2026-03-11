1d61fcec1aca5901f67e3ec9d5c02ed2d28ae06c
unknown <Administrador@V4CE11831VKV.dsm.local>
2026-03-11 11:03:21 -0500

    auto create superuser

--- barberia/apps.py
+++ barberia/apps.py
@@ -2,4 +2,15 @@ from django.apps import AppConfig
 
 
 class BarberiaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'barberia'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@barberia.com",
+                password="admin123"
+            )
--- barberia_system/settings.py
+++ barberia_system/settings.py
@@ -7,9 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = 'django-insecure-pm&=b&o#b#&i2_0na62q_e(u4@)^0%3@p!o@qkelc*r!3^)hi4'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["barberia-system.onrender.com"]
 
 
 # APPLICATIONS

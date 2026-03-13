9dd0b6886d957c70e77145e16219cbbdc53cf89b
unknown <Administrador@V4CE11831VKV.dsm.local>
2026-03-13 11:58:08 -0500

    Corrección import views

--- barberia_system/urls.py
+++ barberia_system/urls.py
@@ -1,27 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
-from . import views
-
-urlpatterns = [
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),  # veremos logout después
-    path("", views.home, name="home"),  # Página principal después del login
-]
-
-def home(request):
-    return redirect('login')  # redirige al login
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # login / logout de Django
+    # login de django
     path('accounts/', include('django.contrib.auth.urls')),
 
-    # página principal -> login
-    path('', home),
-
-    # rutas de la app barberia
-    path('barberia/', include('barberia.urls')),
+    # urls de la app barberia
+    path('', include('barberia.urls')),
 ]
